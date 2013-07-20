338373fba3c8f1ace4e09a49923882cb7209a126
yaomin <yaomin@gmail.com>
2013-07-20 14:16:08 -0500

    Fix the issues on match() in new IRanges.

--- R/select.siteWins.R
+++ R/select.siteWins.R
@@ -1,8 +1,9 @@
 select.siteWins <-
 function(patt, wins, sites, chr) {
-  .wins <- wins[wins%over%sites[[patt]]]
-  .fac <- match(.wins, sites[[patt]])
-  .dt <- data.frame(chr, patt, cbind(.fac, start(.wins)))
-  names(.dt) <- c("chr", "pattern", "ID", "Win")
-  .dt
+    .wins <- wins[wins%over%sites[[patt]]]
+    #  .fac <- match(.wins, sites[[patt]])
+    .fac <- findOverlaps(.wins, sites[[patt]], select="first")
+    .dt <- data.frame(chr, patt, cbind(.fac, start(.wins)))
+    names(.dt) <- c("chr", "pattern", "ID", "Win")
+    .dt
 }
