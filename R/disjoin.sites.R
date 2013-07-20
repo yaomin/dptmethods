3d045bec1e8b45df4283b119596817fb357ac212
yaomin <yaomin@gmail.com>
2013-07-20 13:21:52 -0500

    compatibility issues with %in% in the newer IRanges package

--- R/disjoin.sites.R
+++ R/disjoin.sites.R
@@ -1,21 +1,23 @@
 #' @export
 disjoin.sites <-
 function(site.rl,
-         gaps.within.rl,
-         type=c("any", "start", "end", "two.ends","within","equal")) {
-  type <- match.arg(type)
-  if(type =="two.ends") {
-    ## mtch1 <- subsetByOverlaps(gaps.within.rl, site.rl, type="start")
-    ## mtch2 <- subsetByOverlaps(gaps.within.rl, site.rl, type="end")
-    gaps.mtched <- gaps.within.rl[!gaps.within.rl%over%subsetByOverlaps(gaps.within.rl,
-                                                                      site.rl,
-                                                                      type="within")]
-    ## gaps.mtched <- combine.2rl(mtch1, mtch2)
+	 gaps.within.rl,
+	 type=c("any", "start", "end", "two.ends","within","equal")) {
+    #    browser()
+    type <- match.arg(type)
+    if(type =="two.ends") {
+	gaps.mtched <- gaps.within.rl[!gaps.within.rl%within%site.rl]
   } else {
     gaps.mtched <- subsetByOverlaps(gaps.within.rl, site.rl, type=type)
   }
-  .disjoin <- disjoin(combine.2rl(site.rl, gaps.mtched))
- 
-  out <- .disjoin[!(.disjoin%over%gaps.mtched)]
+  ## site with gaps on either end
+  site.rl.gaps <- site.rl[site.rl%over%gaps.mtched]
+  ## site with nogaps on either end
+  site.rl.nogaps <- site.rl[!site.rl%over%gaps.mtched] 
+  ## disjoin gaps and sites with gaps
+  .disjoin <- disjoin(combine.2rl(site.rl.gaps, gaps.mtched))
+  ## remove the gaps in sites with end gaps 
+  site.rl.gaps.cleaned <- .disjoin[!(.disjoin%over%gaps.mtched)]
+  out <- combine.2rl(site.rl.nogaps, site.rl.gaps.cleaned)
   out
 }
