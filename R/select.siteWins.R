177813c00ae491d7299bb186eda2f1426f975081
yaomin <yaomin@gmail.com>
2013-07-20 14:19:30 -0500

    clean-up

--- R/select.siteWins.R
+++ R/select.siteWins.R
@@ -1,7 +1,6 @@
 select.siteWins <-
 function(patt, wins, sites, chr) {
     .wins <- wins[wins%over%sites[[patt]]]
-    #  .fac <- match(.wins, sites[[patt]])
     .fac <- findOverlaps(.wins, sites[[patt]], select="first")
     .dt <- data.frame(chr, patt, cbind(.fac, start(.wins)))
     names(.dt) <- c("chr", "pattern", "ID", "Win")
