f7f3e8d06acabadbf7486b3ae2241c1d91178887
yaomin <yaomin@gmail.com>
2013-08-21 22:00:57 -0500

    bug fix

--- R/escore.comp3.R
+++ R/escore.comp3.R
@@ -4,16 +4,17 @@ function(x, group, which.p) {
     p.len <- length(which.p)
     if(p.len>1) {
 	stopifnot(nlevels(grp)!=p.len)
-	sapply(seq(p.len),
-	       function(i) apply(x[,grp==levels(grp)[i]],
-				 1,
-				 quantile,
-				 probs=which.p[i]))
+	out <- 	sapply(seq(p.len),
+			      function(i) apply(x[,grp==levels(grp)[i]],
+						1,
+						quantile,
+						probs=which.p[i]))
     } else if(p.len==1) {
-	t(apply(x, 1, function(y) {
-	      tapply(y,
-		     group,
-		     function(z, which.p) quantile(z, p=which.p),
-		     which.p=which.p)}))
+	out <- 	t(apply(x, 1, function(y) {
+			       tapply(y,
+				      group,
+				      function(z, which.p) quantile(z, p=which.p),
+				      which.p=which.p)}))
     } else stop("wrong length for which.p")
+    out
 }
